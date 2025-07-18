fba6869c21df67f1a6af94b10048140106900fb3
wescly-xavier <wescleypt1@gmail.com>
2025-07-18 13:57:21 -0300

    Add .next/static/chunks/src_app_checkout_page_tsx_a993527f._.js 1752857843715

---
+++ .next/static/chunks/src_app_checkout_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_5dac56ba._.js",
+  "static/chunks/node_modules_7b2eeec2._.js"
+],
+    source: "dynamic"
+});
