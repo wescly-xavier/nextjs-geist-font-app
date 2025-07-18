5d4f822290629d277521632cec249e22f7cf3d3e
wescly-xavier <wescleypt1@gmail.com>
2025-07-18 13:57:29 -0300

    Add .next/static/chunks/src_app_products_[id]_page_tsx_a993527f._.js 1752857851314

---
+++ .next/static/chunks/src_app_products_[id]_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_b626a816._.js",
+  "static/chunks/node_modules_5c19302d._.js"
+],
+    source: "dynamic"
+});
